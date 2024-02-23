4f6153157d9c62b2a816c5db7f9f94b708c3b359
Abdeddaim Mansar <mansarabdeddaim12@gmaiL.com>
2024-02-23 16:28:23 +0100

    feat (day 1): add part 2

--- day01/Day01.go
+++ day01/Day01.go
@@ -2,6 +2,7 @@ package day01
 
 import (
 	"advent-of-code-go/util"
+	"sort"
 	"strconv"
 )
 
@@ -25,6 +26,22 @@ func (day Day01) Part1(data []string) string {
 	return strconv.Itoa(totalCalories)
 }
 
-func (day Day01) Part2([]string) string {
-	return ""
+func (day Day01) Part2(data []string) string {
+	var calories = []int{}
+	for i, _ := range data {
+		caloriesPerElve := 0
+		for j := i; j < len(data); j++ {
+			if data[j] == "" {
+				break
+			}
+			caloriesPerElve += util.ParseToInt(data[j])
+		}
+
+		calories = append(calories, caloriesPerElve)
+	}
+	sort.Slice(calories, func(i, j int) bool {
+		return calories[i] > calories[j]
+	})
+	totalOfTopThree := calories[0] + calories[1] + calories[2]
+	return strconv.Itoa(totalOfTopThree)
 }
