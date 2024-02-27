d984e9e917b00ab627be887be158b3065bc84247
Abdeddaim Mansar <mansarabdeddaim12@gmaiL.com>
2024-02-27 16:16:02 +0100

    feat (day 2): add part 2

--- day/day02.go
+++ day/day02.go
@@ -2,8 +2,16 @@ package day
 
 import (
 	"strconv"
+	"strings"
 )
 
+// Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
+// rock: A:X
+// paper: B:Y
+// Scissors : c:Z
+// x -> lose
+// y -> draw
+// z -> win
 var games = map[string]int{
 	"A X": 4,
 	"A Y": 8,
@@ -27,5 +35,33 @@ func (day Day02) Part1(data []string) string {
 }
 
 func (day Day02) Part2(data []string) string {
-	return ""
+	totalScores := 0
+	for _, line := range data {
+		split := strings.Split(line, "")
+		strategy := split[0] + " "
+		if split[0] == "A" {
+			switch split[2] {
+			case "X":
+				strategy += "Z"
+			case "Y":
+				strategy += "X"
+			case "Z":
+				strategy += "Y"
+			}
+		} else if split[0] == "C" {
+			switch split[2] {
+			case "X":
+				strategy += "Y"
+			case "Y":
+				strategy += "Z"
+			case "Z":
+				strategy += "X"
+			}
+		} else {
+			strategy = line
+		}
+		score := games[strategy]
+		totalScores += score
+	}
+	return strconv.Itoa(totalScores)
 }
